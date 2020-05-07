3e178f3878e9cb8c465c7fa2f19fe97f819420f7
marriely <marriely@hotmail.com>
2020-05-06 21:45:39 -0300

    login

---
+++ cypress/integration/login.js
@@ -0,0 +1,32 @@
+/// <reference types='cypress' />
+
+import {EMAIL, SENHA, BTN_ENTRAR} from '../support/elements/loginElements.js';
+
+describe('Realizar login', () => {
+    beforeEach(()=>{
+        cy.fixture('data').as('user').then(()=>{
+
+        })
+        cy.visit('http://barrigareact.wcaquino.me/')
+    })//before
+ 
+
+    it('Login com sucesso', ()=> {
+                
+        cy.get(EMAIL)
+          .type('marriely@hotmail.com')  
+          
+        cy.get(SENHA)
+            .type('M@rriely29')
+
+        cy.get(BTN_ENTRAR)
+            .click()
+
+        cy.get('.toast').should('be.visible')
+            
+
+    })//it login com sucesso
+
+})//describe
+
+
---
+++ cypress/support/elements/loginElements.js
@@ -0,0 +1,3 @@
+export const EMAIL = '[data-test="email"]';
+export const SENHA = '[data-test="passwd"]';
+export const BTN_ENTRAR = '.btn';
